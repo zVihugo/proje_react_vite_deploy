5265126e02ed6e9d9c26df5b02f7239ad135ffef
Victor Moreira <zvihugo@hotmail.com>
2024-08-12 08:44:01 -0300

    Update App.jsx

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -3,8 +3,8 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 //Importando Pages
 import Inicial from './pages/Inicial/Inicial'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
+// import Login from './pages/Login/Login'
+// import Register from './pages/Register/Register'
 
 const App = () => {
   return (
