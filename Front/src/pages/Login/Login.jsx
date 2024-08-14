d4774ef58fe8d8adb39df2a3968baee822353ac2
Victor Moreira <zvihugo@hotmail.com>
2024-08-13 23:00:16 -0300

    .

--- Front/src/pages/Login/Login.jsx
+++ Front/src/pages/Login/Login.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
-import styles from './Login.module.css';
 import {login} from "../../services/authServices";
 
 const Login = () => {
