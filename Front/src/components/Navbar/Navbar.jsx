d93a5a0330d9bb6540df8af909287598e161e2e1
Victor Moreira <zvihugo@hotmail.com>
2024-08-14 14:42:18 -0300

    .

--- Front/src/components/Navbar/Navbar.jsx
+++ Front/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import "./Navbar.module.css";
+import styles from './Navbar.module.css'
+import React from 'react'
+import {NavLink, useLocation} from 'react-router-dom'
 
 
-function NavbarInitial() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary custom-navbar">
-      <Container>
-        <Navbar.Brand href="/Inicial">Dragon Ball</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/Inicial">Home</Nav.Link>
-            <Nav.Link href="/Insertion">Inserir Postagens</Nav.Link>            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
 
-export default NavbarInitial;
+const Navbar = () => {
+
+    return (
+            <nav className={styles.navbar}>
+                
+                <ul className={styles.links_list}>
+                    <li>
+                        <NavLink to="/" className={({isActive}) => `${styles.noEffect} ${isActive ? styles.active : ""}`}>Home</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        )
+    }
+
+export default Navbar
