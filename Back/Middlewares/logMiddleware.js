7e40f7636166b08f76a9bb7b6443eac89f06b33c
Victor Moreira <zvihugo@hotmail.com>
2024-08-15 20:59:54 -0300

    update logMiddleware

--- Back/Middlewares/logMiddleware.js
+++ Back/Middlewares/logMiddleware.js
@@ -2,13 +2,25 @@ const fs = require('fs');
 const path = require('path');
 
 const logMiddleware = (req, res, next) => {
-    const caminho = path.join(__dirname, 'logs', 'security.log');
-    const mensagem = `${new Date().toISOString()} - ${req.method} ${req.originalUrl} - ${req.ip}\n`;
+    const logPath = path.join(__dirname, 'logs', 'security.log');
+    const accessMessage = `${new Date().toISOString()} - ${req.method} ${req.originalUrl} - ${req.ip}\n`;
 
-    fs.appendFile(caminho, mensagem, (err) => {
+  
+    fs.appendFile(logPath, accessMessage, (err) => {
         if (err) {
-            console.error('Erro ao registrar log:', err);
-            
+            console.error('Erro ao registrar log de acesso:', err);
+        }
+    });
+
+  
+    res.on('finish', () => {
+        if (res.statusCode >= 400) {
+            const errorMessage = `${new Date().toISOString()} - ERROR ${res.statusCode} - ${req.method} ${req.originalUrl} - ${req.ip}\n`;
+            fs.appendFile(logPath, errorMessage, (err) => {
+                if (err) {
+                    console.error('Erro ao registrar log de erro:', err);
+                }
+            });
         }
     });
 
